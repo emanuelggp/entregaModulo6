bd119ce0150610c6938b6821687d5682b77a4829
emanuelggp <emanuel.gomez.p@gmail.com>
2024-07-12 14:06:31 -0400

    Base de datos

---
+++ alkewallet.sql
@@ -0,0 +1,116 @@
+CREATE DATABASE  IF NOT EXISTS `alkewallet` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `alkewallet`;
+-- MySQL dump 10.13  Distrib 8.3.0, for Win64 (x86_64)
+--
+-- Host: localhost    Database: alkewallet
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cuenta`
+--
+
+DROP TABLE IF EXISTS `cuenta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cuenta` (
+  `idCuenta` bigint NOT NULL AUTO_INCREMENT,
+  `numeroCuenta` int NOT NULL,
+  `saldo` int NOT NULL,
+  `idUsuario` bigint DEFAULT NULL,
+  PRIMARY KEY (`idCuenta`),
+  KEY `FKtjn6osho229d1l5hnlvb0futb` (`idUsuario`),
+  CONSTRAINT `FKtjn6osho229d1l5hnlvb0futb` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuenta`
+--
+
+LOCK TABLES `cuenta` WRITE;
+/*!40000 ALTER TABLE `cuenta` DISABLE KEYS */;
+INSERT INTO `cuenta` VALUES (1,0,350000,1),(2,0,150000,2),(3,0,0,1),(4,0,0,1),(5,0,0,1),(6,0,0,1),(7,0,0,1),(8,0,0,1),(9,0,0,1),(10,0,0,1),(11,0,0,2),(12,0,0,2),(13,0,0,2),(14,0,0,1),(15,0,0,2),(16,0,95000,3),(17,0,0,3);
+/*!40000 ALTER TABLE `cuenta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transferencia`
+--
+
+DROP TABLE IF EXISTS `transferencia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transferencia` (
+  `idTransferencia` bigint NOT NULL AUTO_INCREMENT,
+  `fecha` datetime(6) DEFAULT NULL,
+  `monto` int NOT NULL,
+  `envio` bigint DEFAULT NULL,
+  `recepcion` bigint DEFAULT NULL,
+  PRIMARY KEY (`idTransferencia`),
+  KEY `FK4i2nnrqjlhvpj2u5h5829s65w` (`envio`),
+  KEY `FKdudokq9un05da99s7i2mld1n` (`recepcion`),
+  CONSTRAINT `FK4i2nnrqjlhvpj2u5h5829s65w` FOREIGN KEY (`envio`) REFERENCES `usuario` (`idUsuario`),
+  CONSTRAINT `FKdudokq9un05da99s7i2mld1n` FOREIGN KEY (`recepcion`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transferencia`
+--
+
+LOCK TABLES `transferencia` WRITE;
+/*!40000 ALTER TABLE `transferencia` DISABLE KEYS */;
+INSERT INTO `transferencia` VALUES (1,'2024-07-10 10:52:52.158000',75000,1,2),(2,'2024-07-10 12:06:04.412000',50000,2,1);
+/*!40000 ALTER TABLE `transferencia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `idUsuario` bigint NOT NULL AUTO_INCREMENT,
+  `apellido` varchar(255) COLLATE utf8mb3_spanish2_ci DEFAULT NULL,
+  `balance` int NOT NULL,
+  `email` varchar(255) COLLATE utf8mb3_spanish2_ci DEFAULT NULL,
+  `nombre` varchar(255) COLLATE utf8mb3_spanish2_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb3_spanish2_ci DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Perez',325000,'jperez@mail.com','Juan','1234'),(2,'Pino',175000,'apino@mail.com','Ana','9876'),(3,'Tapia',95000,'ptapia@mail.com','Pedro','6543');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-12 14:03:29
